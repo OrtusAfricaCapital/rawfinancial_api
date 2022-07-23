5696cccd6c1aa17b29e8d0e6446317bce1a6b50a
Emmanuel Mugenyi <emugenyi@newvision.co.ug>
2022-07-23 17:47:09 +0300

    pick database string from env varibales

--- LMS-V2.API/Startup.cs
+++ LMS-V2.API/Startup.cs
@@ -27,14 +27,14 @@ namespace LMS_V2.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LMSDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("LMSDb-Test")));
+            services.AddDbContext<LMSDbContext>(options => options.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL")));
 
             services.AddControllers();
             services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, LMSDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +42,8 @@ namespace LMS_V2.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            dbContext.Database.Migrate();
 
             app.UseHttpsRedirection();
 
